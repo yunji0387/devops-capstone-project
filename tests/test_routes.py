c142ee83404a948dec58a7debf31cef0f2940b2d
yunji0387 <yunji0387@gmail.com>
2025-07-12 02:39:36 +0800

    Implement GET endpoint for reading an account and add corresponding tests

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -20,6 +20,10 @@ DATABASE_URI = os.getenv(
 BASE_URL = "/accounts"
 
 
+class TestRoutes:
+    def test_something(self):
+        assert True
+
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
@@ -123,4 +127,18 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    # ADD YOUR TEST CASES HERE ...
+    def test_read_an_account(self):
+        """It should Read an Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.get(
+            f"{BASE_URL}/{account.id}", content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], account.name)
+    
+    def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+    
